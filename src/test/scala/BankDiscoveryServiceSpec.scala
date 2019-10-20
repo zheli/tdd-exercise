99b2a81ca7a3dfddda7058a1b62ec7355142b9ad
Zhe Li <linuxcity.jn@gmail.com>
2019-10-20 15:55:00 +0200

    Add another test case.

--- src/test/scala/BankDiscoveryServiceSpec.scala
+++ src/test/scala/BankDiscoveryServiceSpec.scala
@@ -1,9 +1,19 @@
 import org.scalatest._
 
 class BankDiscoveryServiceSpec extends FlatSpec with Matchers {
-    val mockTransactions = Seq.empty[Transaction]
   "BankDiscoveryService.findSubscriptions" should "not crash if pass in empty transaction sequence" in {
-    val result = BankDiscoveryService.findSubscriptions(mockTransactions)
+    val testTransactions = Seq.empty[Transaction]
+    val result = BankDiscoveryService.findSubscriptions(testTransactions)
     result shouldBe None
+  }
+
+  it should "find monthly subscription which withdraws around the same date every month" in {
+    val testData = Seq(
+      Transaction("20160723", "Video streaming", None, -99),
+      Transaction("20160622", "Video streaming", None, -99),
+      Transaction("20160523", "Video streaming", None, -99)
+    )
+    val result = BankDiscoveryService.findSubscriptions(testData)
+    result shouldBe Subscription("Video streaming", 30, 99)
   }
 }
