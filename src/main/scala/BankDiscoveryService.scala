b4f10db57dbdabe1375d365923f426ab2da80de4
Zhe Li <linuxcity.jn@gmail.com>
2019-10-20 17:05:40 +0200

    Change the logic to pass the test.

--- src/main/scala/BankDiscoveryService.scala
+++ src/main/scala/BankDiscoveryService.scala
@@ -1,8 +1,13 @@
 object BankDiscoveryService {
   def findSubscriptions(transactions: Seq[Transaction]): Seq[Subscription] = {
-    val transactionGroups = transactions.groupBy(_.description).toSeq
-    transactionGroups.map {
-      case (text, trans) if trans.length > 1 => Subscription(text, 30, trans.head.amount * -1)
+    val groupByText = transactions.groupBy(_.description).toSeq
+    groupByText.flatMap {
+      case (text, sameTextTrans) if sameTextTrans.length > 1 =>
+        val groupByAmount = sameTextTrans.groupBy(_.amount).toSeq
+        groupByAmount.map {
+          case (amount, sameAmountTrans) if sameAmountTrans.length > 1 =>
+            Subscription(text, 30, amount * -1)
+        }
     }
   }
 }
