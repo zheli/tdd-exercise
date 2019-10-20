367d026961a298ab9a7607c2f2b1cc3094506b9b
Zhe Li <linuxcity.jn@gmail.com>
2019-10-20 16:59:09 +0200

    Add logic to pass the second test.

--- src/main/scala/BankDiscoveryService.scala
+++ src/main/scala/BankDiscoveryService.scala
@@ -1,5 +1,8 @@
 object BankDiscoveryService {
-  def findSubscriptions(transactions: Seq[Transaction]): Option[Subscription] = {
-    None
+  def findSubscriptions(transactions: Seq[Transaction]): Seq[Subscription] = {
+    val transactionGroups = transactions.groupBy(_.description).toSeq
+    transactionGroups.map {
+      case (text, trans) if trans.length > 1 => Subscription(text, 30, trans.head.amount * -1)
+    }
   }
 }
--- src/test/scala/BankDiscoveryServiceSpec.scala
+++ src/test/scala/BankDiscoveryServiceSpec.scala
@@ -4,16 +4,16 @@ class BankDiscoveryServiceSpec extends FlatSpec with Matchers {
   "BankDiscoveryService.findSubscriptions" should "not crash if pass in empty transaction sequence" in {
     val testTransactions = Seq.empty[Transaction]
     val result = BankDiscoveryService.findSubscriptions(testTransactions)
-    result shouldBe None
+    result shouldBe Seq.empty
   }
 
   it should "find monthly subscription which withdraws around the same date every month" in {
     val testData = Seq(
-      Transaction("20160723", "Video streaming", None, -99),
-      Transaction("20160622", "Video streaming", None, -99),
-      Transaction("20160523", "Video streaming", None, -99)
+      Transaction("2016-07-23", "Video streaming", None, -99),
+      Transaction("2016-06-22", "Video streaming", None, -99),
+      Transaction("2016-05-23", "Video streaming", None, -99)
     )
     val result = BankDiscoveryService.findSubscriptions(testData)
-    result shouldBe Subscription("Video streaming", 30, 99)
+    result shouldBe Seq(Subscription("Video streaming", 30, 99))
   }
 }
